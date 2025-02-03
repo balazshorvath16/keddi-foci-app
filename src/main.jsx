17f4376a487b8e2b2839d37ce283263a0dc26bf6
balazshorvath16 <balazs.horvath1313@gmail.com>
2025-02-03 16:03:14 +0100

    Push értesítés bugfix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import { register } from './serviceWorker';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker regisztrálva:', registration);
+    })
+    .catch((err) => {
+      console.error('Service Worker regisztrációs hiba:', err);
+    });
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
